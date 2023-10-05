2a1355197f9dcbe2c7f4c13f97512cbfd20be234
MadhanV <madhanbalajiv@outlook.com>
2023-10-05 21:54:01 +0530

    Casting in Java file

---
+++ Casting.java
@@ -0,0 +1,16 @@
+public class Casting{
+    public static void main(String[] args) {
+        System.out.println();
+
+        byte mtMinByteValue = Byte.MIN_VALUE;
+        System.out.println(mtMinByteValue);
+        byte myNewByteValue =(byte) (mtMinByteValue/2); //Casting variable since java treats it as int and throws error.
+        System.out.println("My NewByte Value is "+myNewByteValue);
+
+        //Summary:
+        /*
+         * Variable are generally int 
+         * short mtShort = (-128/2)  work
+         */
+    }
+}
