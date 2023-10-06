f54ee138882b05ef457eccfcbc4fb7966c664a3e
MadhanV <madhanbalajiv@outlook.com>
2023-10-07 00:56:07 +0530

    Added return inside the loop

--- basics/quiz/megaBytesconverter.java
+++ basics/quiz/megaBytesconverter.java
@@ -2,12 +2,13 @@ package basics.quiz;
 
 public class megaBytesconverter {
     public static void main(String[] args) {
-        printMegaBytesAndKiloBytes(258);
+        printMegaBytesAndKiloBytes(-5);
     }
 
     public static void printMegaBytesAndKiloBytes(int kiloBytes){
         if(kiloBytes<0){
             System.out.println("Invalid value");
+            return;
         }
 
         int remainingKiloBytes = kiloBytes%1024;
